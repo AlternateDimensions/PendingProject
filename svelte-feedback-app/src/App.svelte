e3e9b985c2d5377eec6289bed62454632ee13972
notJaceb notJaggedinske <fnorris06@ladueschools.net>
2023-01-18 18:36:33 +0000

    Learn svelte

--- svelte-feedback-app/src/App.svelte
+++ svelte-feedback-app/src/App.svelte
@@ -1,30 +1,29 @@
 <script>
-	export let name;
+	import FeedbackList from './components/FeedbackList.svelte';
+
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: "This is pretty good",
+		},
+		{	id: 2,
+			rating: 9,
+			text: "This is not for me but still decent",
+		},
+		{
+			id: 3,
+			rating: 2,
+			text: "What is this crap????",
+		}
+	]
+	const deleteFeedback = (e) => {
+		const itemId = e.detail
+		feedback = feedback.filter((item) => item.id != itemId)
+
+	}
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<main class="container">
+	<FeedbackList feedback={feedback} on:delete-feedback={deleteFeedback} />
 </main>
-
-<style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
-</style>
---
+++ svelte-feedback-app/src/components/Card.svelte
@@ -0,0 +1,14 @@
+<div class="card">
+    <slot></slot>
+</div>
+
+<style>
+    .card {
+        background: #fff;
+        border-radius: 15px;
+        padding: 35px 100px;
+        margin: 20px 0;
+        display: flex;
+        position: relative;
+    }
+</style>
